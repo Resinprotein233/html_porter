2c0705cfe77175bf6a48d042de484b3ef12eafd6
ZRY-233 <58623137+ZRY-233@users.noreply.github.com>
2020-08-13 15:44:19 +0800

    Update html_porter.py

--- html_porter.py
+++ html_porter.py
@@ -1,7 +1,13 @@
 #python3
 import urllib.request
+kaitou='''
+             HTML_PORTER
+                  V1.0
+本程序默认保存路径为：
+/root/html_porter/
+'''
+print(kaitou)
 url = input("Website_URL->")
-save = input("")
 
 #打开一个网页链接
 htmlhandler = urllib.request.urlopen(url)
@@ -9,7 +15,8 @@ htmlhandler = urllib.request.urlopen(url)
 #在本机上创造一个新文件
 file = open("//root//html_porter//save.html", "wb")
 
-#将网页储存在本机文件里，每次读取512个字节
+#将网页储存在本机文件里，每次读取512个字节,并告示使用者
+print("正在保存中，请稍候....")
 while 1:
    deta = htmlhandler.read(512)
    if not deta:
@@ -21,3 +28,7 @@ file.close()
 
 #关闭网页文件
 htmlhandler.close()
+
+#提示使用者文件保存完毕，并退出程序
+print("保存完成！")
+exit()
